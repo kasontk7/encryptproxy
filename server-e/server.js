079c2ecd78b91c7de16c4e778fe05ba227632261
Kason <kason@kasons-mbp.lan>
2023-08-01 22:08:26 -0700

    Working proxy and server

--- server-e/server.js
+++ server-e/server.js
@@ -14,7 +14,9 @@ var bodyParser = require("body-parser");
 const fs = require("fs");
 const crypto = require("crypto");
 const mime = require("mime");
-var path = require("path");
+const multer = require('multer');
+const sqlite3 = require('sqlite3').verbose();
+const path = require("path");
 
 const data = {
   1: "NYT.txt",
@@ -29,7 +31,68 @@ const data = {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080; // set our port
+var port = 8080; // set our port
+
+// SQLite Database Setup
+const db = new sqlite3.Database('database.db');
+db.serialize(() => {
+  db.run('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, fileName TEXT)');
+});
+
+// Multer Configuration
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage });
+
+
+// var router = express.Router();              // get an instance of the express Router
+// // REGISTER OUR ROUTES -------------------------------
+// // all of our routes will be prefixed with /api
+// app.use('/api', router);
+
+// ROUTES FOR OUR API
+// =============================================================================
+// Upload File Endpoint
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  const { filename } = req.file;
+
+  db.run('INSERT INTO files (fileName) VALUES (?)', [filename], (err) => {
+    if (err) {
+      console.error('Error inserting file data into the database:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(200).json({ message: 'File uploaded successfully!' });
+  });
+});
+
+// Download File Endpoint
+app.get('/api/download/:fileName', (req, res) => {
+  const fileName = req.params.fileName;
+  const filePath = path.join(__dirname, 'uploads', fileName);
+
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      console.error('Error downloading the file:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+});
+
+// Get All Files Endpoint
+app.get('/api/files', (req, res) => {
+  db.all('SELECT fileName FROM files', (err, rows) => {
+    if (err) {
+      console.error('Error fetching files from the database:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    const files = rows.map((row) => row.fileName);
+    res.status(200).json(files);
+  });
+});
 
 // START THE SERVER
 // =============================================================================
--- server-m/proxy.js
+++ server-m/proxy.js
@@ -1,10 +1,18 @@
 // Dependencies
 const http = require("http");
+const request = require('request');
+
+const proxyServerPort = 3001;
+const backendServerPort = 8080;
 
 // Create a server
-const server = http.createServer((downstreamReq, downstreamRes) => {});
+const server = http.createServer((req, res) => {
+    const backendUrl = `http://localhost:${backendServerPort}${req.url}`;
+    req.pipe(request(backendUrl)).pipe(res);
+});
 
 // Listen on port 3001
-server.listen(3001);
+server.listen(proxyServerPort);
+console.log("Server running on port " + proxyServerPort);
 
 module.exports = () => server.listening;
