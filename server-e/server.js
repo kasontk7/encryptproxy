55d34c37bfeeb7293f25d1ef27b3d4a41d088c76
Kason <kason@kasons-mbp.lan>
2023-08-03 03:15:53 -0700

    Modified divs a little

--- server-e/server.js
+++ server-e/server.js
@@ -23,11 +23,11 @@ app.use(cors());
 let sharedSecret;
 
 const data = {
-  1: "NYT.txt",
-  2: "patreon.mp4",
-  3: "Earth.jpg",
-  4: "big.txt",
-  5: "png-test.png"
+	1: "NYT.txt",
+	2: "patreon.mp4",
+	3: "Earth.jpg",
+	4: "big.txt",
+	5: "png-test.png"
 };
 
 // configure app to use bodyParser()
@@ -40,126 +40,140 @@ var port = 8080; // set our port
 // SQLite Database Setup
 const db = new sqlite3.Database('database.db');
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, fileName TEXT)');
-  // Initialize the database based on the current contents of the uploads folder
-  // (We don't files to appear from last usage of db)
-  resetDatabaseFromUploadsFolder();
+	db.run('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, fileName TEXT)');
+	// Initialize the database based on the current contents of the uploads folder
+	// (We don't files to appear from last usage of db)
+	resetDatabaseFromUploadsFolder();
 });
 
 // ROUTES FOR OUR API
 // =============================================================================
 // Upload File Endpoint
 app.post('/api/upload', (req, res) => {
-  const encryptedFile = Buffer.from(req.body.data, 'base64');
-  const iv = Buffer.from(req.body.iv, 'base64');
-  const authTag = Buffer.from(req.body.authTag, 'base64');
-  const decryptedFile = decryptData(encryptedFile, iv, authTag);
-  const filename = `${Date.now()}_${req.body.filename}`;
-  const filePath = path.join(__dirname, "uploads", filename);
-
-  fs.writeFile(filePath, decryptedFile, 'binary', (err) => {
-    if (err) {
-      console.error("Error writing file to disk:", err);
-      return res.status(500).json({ error: "Failed to save file" });
-    }
-    db.run('INSERT INTO files (fileName) VALUES (?)', [filename], (err) => {
-      if (err) {
-        console.error('Error inserting file data into the database:', err);
-        return res.status(500).json({ error: 'Failed to add file to db' });
-      }
-    });
-  res.status(200).json({ message: 'File uploaded successfully!' });
+	const encryptedFile = Buffer.from(req.body.data, 'base64');
+	const iv = Buffer.from(req.body.iv, 'base64');
+	const authTag = Buffer.from(req.body.authTag, 'base64');
+	const decryptedFile = decryptData(encryptedFile, iv, authTag);
+	const filename = req.body.filename;
+	const filePath = path.join(__dirname, "uploads", filename);
+  
+	// Check if the filename already exists in the database
+	db.get('SELECT fileName FROM files WHERE fileName = ?', [filename], (err, row) => {
+	  if (err) {
+		console.error('Error querying the database:', err);
+		return res.status(500).json({ error: 'Failed to check filename in db' });
+	  }
+  
+	  if (row) {
+		// The filename already exists in the database
+		return res.status(400).json({ error: 'Filename already exists in the database' });
+	  }
+  
+	  fs.writeFile(filePath, decryptedFile, 'binary', (err) => {
+		if (err) {
+		  console.error("Error writing file to disk:", err);
+		  return res.status(500).json({ error: "Failed to save file" });
+		}
+		db.run('INSERT INTO files (fileName) VALUES (?)', [filename], (err) => {
+		  if (err) {
+			console.error('Error inserting file data into the database:', err);
+			return res.status(500).json({ error: 'Failed to add file to db' });
+		  }
+		  res.status(200).json({ message: 'File uploaded successfully!' });
+		});
+	  });
+	});
   });
-});
+  
 
 // Download File Endpoint
 app.get('/api/download/:fileName', (req, res) => {
-  const fileName = req.params.fileName;
-  const filePath = path.join(__dirname, 'uploads', fileName);
+	const fileName = req.params.fileName;
+	const filePath = path.join(__dirname, 'uploads', fileName);
 
-  fs.readFile(filePath, (err, fileContent) => {
-    if (err) {
-      console.error('Error reading the file:', err);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-    const iv = crypto.randomBytes(16);
-    const { authTag, encryptedData } = encryptData(fileContent, iv);
+	fs.readFile(filePath, (err, fileContent) => {
+		if (err) {
+			console.error('Error reading the file:', err);
+			return res.status(500).json({ error: 'Internal Server Error' });
+		}
+		const iv = crypto.randomBytes(16);
+		const { authTag, encryptedData } = encryptData(fileContent, iv);
 
-    res.status(200).json({ 
-      authTag: authTag.toString('base64'),
-      data: encryptedData.toString('base64'),
-      iv: iv.toString('base64'),
-    });
-  });
+		res.status(200).json({ 
+			authTag: authTag.toString('base64'),
+			data: encryptedData.toString('base64'),
+			iv: iv.toString('base64'),
+		});
+	});
 });
 
 // Get All Files Endpoint
 app.get('/api/files', (req, res) => {
-  db.all('SELECT fileName FROM files', (err, rows) => {
-    if (err) {
-      console.error('Error fetching files from the database:', err);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-    const files = rows.map((row) => row.fileName);
-    res.status(200).json(files);
-  });
+	db.all('SELECT fileName FROM files', (err, rows) => {
+		if (err) {
+			console.error('Error fetching files from the database:', err);
+			return res.status(500).json({ error: 'Internal Server Error' });
+		}
+		const files = rows.map((row) => row.fileName);
+		res.status(200).json(files);
+	});
 });
 
 app.post('/api/exchange-keys', (req, res) => {
-  const frontendPublicKey = Buffer.from(req.body.publicKey,'base64');
-  const ecdhCurve = crypto.createECDH('secp521r1');
-  ecdhCurve.generateKeys();
-  const backendPublicKeyBuffer = ecdhCurve.getPublicKey();
-  const sharedSecretKey = ecdhCurve.computeSecret(frontendPublicKey);
-  sharedSecret = crypto.createHmac('sha256', sharedSecretKey).update('encryption key').digest();
-  // Send the backend's public key to the frontend
-  res.status(200).json({ publicKey: backendPublicKeyBuffer.toString('base64') });
+	const frontendPublicKey = Buffer.from(req.body.publicKey,'base64');
+	const ecdhCurve = crypto.createECDH('secp521r1');
+	ecdhCurve.generateKeys();
+	const backendPublicKeyBuffer = ecdhCurve.getPublicKey();
+	const sharedSecretKey = ecdhCurve.computeSecret(frontendPublicKey);
+	sharedSecret = crypto.createHmac('sha256', sharedSecretKey).update('encryption key').digest();
+	// Send the backend's public key to the frontend
+	res.status(200).json({ publicKey: backendPublicKeyBuffer.toString('base64') });
 });
 
 function resetDatabaseFromUploadsFolder() {
-  const uploadFolderPath = path.join(__dirname, 'uploads');
+	const uploadFolderPath = path.join(__dirname, 'uploads');
 
-  fs.readdir(uploadFolderPath, (err, files) => {
-    if (err) {
-      console.error('Error reading upload folder:', err);
-      return;
-    }
+	fs.readdir(uploadFolderPath, (err, files) => {
+		if (err) {
+			console.error('Error reading upload folder:', err);
+			return;
+		}
 
-    db.run('DELETE FROM files', (err) => {
-      if (err) {
-        console.error('Error deleting data from the database:', err);
-        return;
-      }
+		db.run('DELETE FROM files', (err) => {
+			if (err) {
+				console.error('Error deleting data from the database:', err);
+				return;
+			}
 
-      files.forEach((fileName) => {
-        db.run('INSERT INTO files (fileName) VALUES (?)', [fileName], (err) => {
-          if (err) {
-            console.error('Error inserting file data into the database:', err);
-          }
-        });
-      });
-    });
-  });
+			files.forEach((fileName) => {
+				db.run('INSERT INTO files (fileName) VALUES (?)', [fileName], (err) => {
+					if (err) {
+						console.error('Error inserting file data into the database:', err);
+					}
+				});
+			});
+		});
+	});
 }
 
 function encryptData(data, iv) {
-  const cipher = crypto.createCipheriv('aes-256-gcm', sharedSecret, iv); 
-  let encryptedData = Buffer.concat([
-      cipher.update(data),
-      cipher.final(),
-      ]);
-  const authTag = cipher.getAuthTag();
-  return { authTag, encryptedData };
+	const cipher = crypto.createCipheriv('aes-256-gcm', sharedSecret, iv); 
+	let encryptedData = Buffer.concat([
+			cipher.update(data),
+			cipher.final(),
+			]);
+	const authTag = cipher.getAuthTag();
+	return { authTag, encryptedData };
 }
 
 function decryptData(data, iv, authTag) {
-  const decipher = crypto.createDecipheriv('aes-256-gcm', sharedSecret, iv);
-  decipher.setAuthTag(authTag);
-  let decryptedFile = Buffer.concat([
-    decipher.update(data),
-    decipher.final(),
-  ]);
-  return decryptedFile;
+	const decipher = crypto.createDecipheriv('aes-256-gcm', sharedSecret, iv);
+	decipher.setAuthTag(authTag);
+	let decryptedFile = Buffer.concat([
+		decipher.update(data),
+		decipher.final(),
+	]);
+	return decryptedFile;
 }
 
 // START THE SERVER
--- server-m/proxy.js
+++ server-m/proxy.js
@@ -1,34 +1,90 @@
-// Dependencies
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-var cors = require('cors');
+const bodyParser = require("body-parser");
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
-
-const backendUrl = 'http://localhost:8080'; // Replace with the backend server URL
-
-app.use('/api', createProxyMiddleware({ target: backendUrl, changeOrigin: true }));
-
 const port = 3001;
-app.listen(port, () => {
-  console.log(`Proxy server is running on port ${port}`);
+const backendUrl = 'http://localhost:8080';
+const uploadsPath = path.join(__dirname, 'uploads');
+const downloadsPath = path.join(__dirname, 'downloads');
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+// Intercept the '/api/upload' endpoint
+app.post('/api/upload', async (req, res) => {
+	const { data, iv, authTag, filename } = req.body;
+	try {
+		await axios.post(`${backendUrl}/api/upload`, {
+			data: data,
+			iv: iv,
+			authTag: authTag,
+			filename: filename,
+		});
+		
+	} catch (error) {
+		return res.status(500).json({ error: 'Failed to upload file' });
+	}
+	// Store the encrypted file in the uploads folder
+	const filePath = path.join(uploadsPath, filename);
+	fs.writeFile(filePath, data, 'binary', (err) => {
+		if (err) {
+			console.error('Error writing file to disk:', err);
+			return res.status(500).json({ error: 'Failed to save file in proxy layer' });
+		}
+		res.status(200).json({ message: 'Encrypted data from upload stored successfully!' });
+	});
 });
 
-// const http = require("http");
-// const request = require('request');
+// Intercept the '/api/download/:fileName' endpoint
+app.get('/api/download/:fileName', async (req, res) => {
+	const fileName = req.params.fileName;
+	const response = await axios.get(`${backendUrl}/api/download/${fileName}`);
+	const { data, iv, authTag } = response.data;
 
-// const proxyServerPort = 3001;
-// const backendServerPort = 8080;
+	// Store the encrypted file in the downloads folder
+	const filePath = path.join(downloadsPath, fileName);
+	fs.writeFile(filePath, data, 'binary', (err) => {
+		if (err) {
+			console.error('Error writing file to disk:', err);
+			return res.status(500).json({ error: 'Failed to save file in proxy layer' });
+		}
+		res.status(200).json({
+			authTag: authTag,
+			data: data,
+			iv: iv,
+			message: 'Encrypted data from download stored successfully!'
+		});
+	});
+});
 
-// // Create a server
-// const server = http.createServer((req, res) => {
-//     const backendUrl = `http://localhost:${backendServerPort}${req.url}`;
-//     req.pipe(request(backendUrl)).pipe(res);
-// });
+// Intercept exchange keys
+app.post('/api/exchange-keys', async (req, res) => {
+	try {
+		const response = await axios.post(`${backendUrl}/api/exchange-keys`, {
+			publicKey: req.body.publicKey,
+		});
+		res.status(200).json({ publicKey: response.data.publicKey });
+	} catch (error) {
+		res.status(500).json({ error: 'Failed to exchange keys with the backend server' });
+	}
+});
 
-// // Listen on port 3001
-// server.listen(proxyServerPort);
-// console.log("Server running on port " + proxyServerPort);
+// Intercept fetch files
+app.get('/api/files', async (req, res) => {
+	try {
+		const response = await axios.get(`${backendUrl}/api/files`);
+		res.status(200).json(response.data);
+	} catch (error) {
+		res.status(500).json({ error: 'Failed to fetch files from the backend server' });
+	}
+});
 
-// module.exports = () => server.listening;
+app.listen(port, () => {
+	console.log(`Proxy server running on port ${port}`);
+});
+
